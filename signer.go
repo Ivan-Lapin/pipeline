45c0a59edbb07304cbd3c10d02efcb372674441e
Ivan Lapin <104844376+Ivan-Lapin@users.noreply.github.com>
2025-03-17 00:41:16 +0300

    ExecutePipeline function

--- signer.go
+++ signer.go
@@ -1,21 +1,46 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
 // сюда писать код
 
-func SingleHash() {
+func SingleHash(in, out chan interface{}) {
 
 }
 
-func MultiHash() {
+func MultiHash(in, out chan interface{}) {
 
 }
 
-func CombineResults() {
+func CombineResults(in, out chan interface{}) {
 
 }
 
-func ExecutePipeline() {
+func ExecutePipeline(jobs ...job) {
+	var wg sync.WaitGroup
 
+	in := make(chan interface{})
+
+	for i, work := range jobs {
+
+		out := make(chan interface{})
+
+		wg.Add(1)
+		go func(fn job, inCh chan interface{}, outCh chan interface{}) {
+			defer wg.Done()
+			defer close(outCh)
+			fn(inCh, outCh)
+		}(work, in, out)
+
+		in = out
+
+		fmt.Printf("Job %d worked\n", i)
+	}
+
+	wg.Wait()
 }
 
 func main() {
