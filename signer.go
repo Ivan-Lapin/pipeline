a189e5adb5d940de6382749e48cd1eba4574d1aa
Ivan Lapin <104844376+Ivan-Lapin@users.noreply.github.com>
2025-03-20 23:51:11 +0300

    Function improvement, but not enough fast

--- signer.go
+++ signer.go
@@ -6,17 +6,21 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"sync/atomic"
+	"time"
 )
 
 func SingleHash(in, out chan interface{}) {
 	var m sync.Mutex
-	var wg sync.WaitGroup
-	index := 0
+	//var wg sync.WaitGroup
+	var index int32
+	done := make(chan struct{})
 
+	start := time.Now()
 	for val := range in {
-		wg.Add(1)
-		go func(value interface{}, outCh chan interface{}, idx int) {
-			defer wg.Done()
+		//wg.Add(1)
+		go func(value interface{}, outCh chan interface{}, cancel chan struct{}) {
+			//defer wg.Done()
 			data_int, _ := value.(int)
 			data := strconv.Itoa(data_int)
 			data_crc32 := DataSignerCrc32(data)
@@ -34,30 +38,48 @@ func SingleHash(in, out chan interface{}) {
 			fmt.Printf("%v  SingleHash crc32(data) %v\n", data, data_crc32)
 			fmt.Printf("%v  SingleHash result %v\n", data, result)
 			outCh <- result
-		}(val, out, index)
-		index++
+			cancel <- struct{}{}
+
+		}(val, out, done)
+		atomic.AddInt32(&index, 1)
 	}
 
-	wg.Wait()
+	// wg.Wait()
+	for i := 0; i < int(index); i++ {
+		<-done
+	}
+	end := time.Since(start)
+	fmt.Printf("Time: %v\n", end)
 }
 
 func MultiHash(in, out chan interface{}) {
 	var wg sync.WaitGroup
+	mapCh := sync.Map{}
+	done := make(chan struct{})
 
 	for val := range in {
 		wg.Add(1)
 		go func(inVal interface{}, out chan interface{}) {
 			defer wg.Done()
 			result := ""
+			go func() {
+				for i := 0; i < 6; i++ {
+					num := strconv.Itoa(i)
+					value, _ := inVal.(string)
+					data := num + value
+					data_crc32 := DataSignerCrc32(data)
+					result += data_crc32
+					mapCh.Store(i, result)
+					done <- struct{}{}
+				}
+			}()
 			for i := 0; i < 6; i++ {
-				num := strconv.Itoa(i)
-				value, _ := inVal.(string)
-				data := num + value
-				data_crc32 := DataSignerCrc32(data)
-				result += data_crc32
-				fmt.Printf("%v MultiHash: crc32(th+step1) %d %s\n", inVal, i, data_crc32)
+				<-done
+				val, _ := mapCh.Load(i)
+				fmt.Printf("%v MultiHash: crc32(th+step1) %d %v\n", inVal, i, val)
 			}
 			fmt.Printf("%v MultiHash result %s\n", inVal, result)
+			out <- result
 		}(val, out)
 	}
 
@@ -78,11 +100,10 @@ func CombineResults(in, out chan interface{}) {
 		}(val, out)
 	}
 	wg.Wait()
-
 	sort.Strings(slice)
 	result := strings.Join(slice, "_")
 	out <- result
-	fmt.Printf("CombineResults\n%s", result)
+	fmt.Printf("CombineResults\n%s\n", result)
 }
 
 func ExecutePipeline(jobs ...job) {
@@ -92,9 +113,7 @@ func ExecutePipeline(jobs ...job) {
 	close(in)
 
 	for i, work := range jobs {
-
 		out := make(chan interface{})
-
 		wg.Add(1)
 		go func(fn job, inCh chan interface{}, outCh chan interface{}, num int) {
 			defer wg.Done()
@@ -103,7 +122,6 @@ func ExecutePipeline(jobs ...job) {
 		}(work, in, out, i)
 
 		in = out
-
 	}
 
 	wg.Wait()
